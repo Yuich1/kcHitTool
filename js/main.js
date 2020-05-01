fe99bf4a9de2bebb369e52cf8e8be8c0ea4d1bab
Yuich1 <51749661+Yuich1@users.noreply.github.com>
2020-05-01 15:50:05 +0900

    add item (id: 373, 374)
    夜攻天山二機を追加
    雷装装備ボーナスを反映

--- js/main.js
+++ js/main.js
@@ -289,7 +289,8 @@ $(function () {
                             selectedItemList[slotNumber] = item;
                             const ta = getMultiBonus(selectedItemList);
                             const bonusPower = (r.power ? r.power : 0) + (s.power ? s.power : 0) + (ta.power ? ta.power : 0) - (t.power ? t.power : 0);
-                            const title = `${item.power ? `火力 ${item.power}` : `${bonusPower != 0 ? "火力 " : ""}`}${bonusPower != 0 ? `(${bonusPower > 0 ? "+" : ""}${bonusPower}), ` : `${item.power ? ", " : ""}`}${item.bomb ? `爆装 ${item.bomb}, ` : ""}${item.torp ? `雷装 ${item.torp}, ` : ""}${item.accuracy ? `命中 ${item.accuracy}` : ""}`;
+                            const bonusTorp = (r.torp ? r.torp : 0) + (s.torp ? s.torp : 0) + (ta.torp ? ta.torp : 0) - (t.torp ? t.torp : 0);
+                            const title = `${item.power ? `火力 ${item.power}` : `${bonusPower != 0 ? "火力 " : ""}`}${bonusPower != 0 ? `(${bonusPower > 0 ? "+" : ""}${bonusPower}), ` : `${item.power ? ", " : ""}`}${item.bomb ? `爆装 ${item.bomb}, ` : ""}${item.torp ? `雷装 ${item.torp}` : ""}${bonusTorp != 0 ? `(+${bonusTorp}), ` : ", "}${item.accuracy ? `命中 ${item.accuracy}` : ""}`;
                             $(this).children(".item-tooltip").attr("title", title).tooltip("fixTitle").tooltip("show");
                             $('[data-toggle="tooltip"]').tooltip();
                         }
@@ -331,6 +332,7 @@ const setItem = (slotNumber, item) => {
     const singleBonus = item.singleBonus ? getSingleBonus(item, slotNumber) : 0;
     const multiBonus = getMultiBonus(selectedItemList);
     selectedItemList[slotNumber].power = (selectedItemList[slotNumber].power ? selectedItemList[slotNumber].power : 0) + (singleAddableBonus.power ? singleAddableBonus.power : 0) + (singleBonus.power ? singleBonus.power : 0);
+    selectedItemList[slotNumber].torp = (selectedItemList[slotNumber].torp ? selectedItemList[slotNumber].torp : 0) + (singleAddableBonus.torp ? singleAddableBonus.torp : 0) + (singleBonus.torp ? singleBonus.torp : 0);
 
     selectedItemList.forEach((t) => {
         if (t != 0) {
